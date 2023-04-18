b14cd19dd8a39fecf510d60c0cda182208924be7
Anujkumar Yadav <anujkumaryadav.in@gmail.com>
2023-04-19 02:28:26 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import ResumeState from './Context/ResumeState';
+import { Routes, Route } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Templates from './Pages/Templates/Templates'
+import Tips from './Pages/Tips/Tips'
+import Navbar from './Components/Navbar/Navbar';
+import About from './Pages/About/About';
+
+function App() {
+
+  return (
+    <ResumeState>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/templates" element={<Templates />} />
+          <Route exact path="/tips" element={<Tips />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </ResumeState>
+  );
+}
+
+export default App;
---
+++ src/Pages/Tips/Tips.jsx
@@ -0,0 +1,8 @@
+
+
+export default function Tips(){
+
+    return(
+        <h1>Tips</h1>
+    );
+}
